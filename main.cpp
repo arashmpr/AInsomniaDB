6eeea64fb0db3f94200d2e0ba358c89e838eb298
arashmpr <arashmdp@gmail.com>
2024-01-25 19:17:38 +0330

    Added a shell and it's working fine

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include <interface.h>
 #include <command_handler.h>
+#include <config.h>
 
 int main(int argc, char* argv[]) {
-  CommandBuffer* command_buffer = new_command_buffer();
-  while (true) {
-    print_insomniadb_prompt();
-    read_input(command_buffer);
+    CommandBuffer* command_buffer = new_command_buffer();
+    std::cout << "InsomniaDB Version " << InsomniaDB_VERSION_MAJOR << "." << InsomniaDB_VERSION_MINOR << std::endl;
+    printf("Type 'help', 'copyright', 'credits' or license' for more information.\n");
+    while (true) {
+        print_insomniadb_prompt();
+        read_input(command_buffer);
 
-    char* command_type = parse_command_type(command_buffer->command);
-    if (strcmp(command_type, EXIT) == 0) {
-        exit(EXIT_SUCCESS);
-    } else if (strcmp(command_type, HELP) == 0) {
-        std::cout << "Error: Right now we don't have any help. sorry." << std::endl;
-    } else {
-        printf("Unrecognized command '%s'.\n", command_buffer->command);
+        char* command_type = parse_command_type(command_buffer->command);
+        if (strcmp(command_type, EXIT) == 0) {
+            exit(EXIT_SUCCESS);
+        } else if (strcmp(command_type, HELP) == 0) {
+            std::cout << "Error: Right now we don't have any help. sorry." << std::endl;
+        } else {
+            printf("Unrecognized command '%s'.\n", command_buffer->command);
+        }
     }
-  }
 }
